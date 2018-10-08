6f13f6f93495850360ca549a4bfbb5a97327da6a
egor-bogomolov <msicar.ot.on.yas@gmail.com>
2018-10-08 18:32:57 +0300

    Description. Iterating over nodes.

--- code/babelfish-sandbox/single_java_fun.py
+++ code/babelfish-sandbox/single_java_fun.py
@@ -1,5 +1,12 @@
+""" Find a method declaration in a file with single class containing single method.
+"""
 import bblfsh
 
 client = bblfsh.BblfshClient("0.0.0.0:9432")
 uast = client.parse("data/SingleFunction.java", language="java").uast
-print(uast)
+# print(uast)
+
+it = bblfsh.iterator(uast, bblfsh.TreeOrder.PRE_ORDER)
+for node in it:
+    if node.internal_type == "MethodDeclaration":
+        print(node)
