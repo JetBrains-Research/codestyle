3f52a29a7659e6259760bca93718e7f2e764ce22
Vladimir Kovalenko <v.v.kovalenko@tudelft.nl>
2018-10-09 20:02:03 +0200

    measure parsing speed and success rate
    (~250 ms/file; ~95% successful)

--- code/python-miner/process_repo.py
+++ code/python-miner/process_repo.py
@@ -1,8 +1,14 @@
+import bblfsh
+import time
 from git import Repo
+
+client = bblfsh.BblfshClient("0.0.0.0:9432")
 
 
 def get_changes(commit, parent):
     diff_index = commit.diff(parent)
+    requests = 0
+    successful_requests = 0
     for entry in diff_index:
 
         # Only look at modified files for now
@@ -15,13 +21,28 @@ def get_changes(commit, parent):
         if not entry.a_path.endswith(".java"):
             continue
 
-        print("NEW CONTENTS:")
-        print(entry.a_blob.data_stream.read().decode('utf-8'))
+        new_contents = entry.a_blob.data_stream.read()
+        # print("NEW CONTENTS:")
+        # print(new_contents)
 
-        print("OLD CONTENTS:")
-        print(entry.b_blob.data_stream.read().decode('utf-8'))
+        new_parse_response = client.parse(filename="", contents=new_contents, language="java")
+        requests += 1
+        if new_parse_response.status == 0:
+            successful_requests += 1
+
+        old_contents = entry.b_blob.data_stream.read()
+        # print("OLD CONTENTS:")
+        # print(old_contents)
+
+        old_parse_response = client.parse(filename="", contents=old_contents, language="java")
+        requests += 1
+        if old_parse_response.status == 0:
+            successful_requests += 1
+        # print(old_parse_response.status)
 
         # TODO persist the contents
+
+    return requests, successful_requests
 
 
 def process_commit(commit):
@@ -31,7 +52,7 @@ def process_commit(commit):
         return
 
     parent = parents[0]
-    get_changes(commit, parent)
+    return get_changes(commit, parent)
 
 
 def process(path):
@@ -39,11 +60,22 @@ def process(path):
 
     commits = repo.iter_commits()
     count = 0
+    limit = 100
+    start = time.time()
+    total = 0
+    success = 0
     for c in commits:
-        process_commit(c)
+        req, succ = process_commit(c)
+        total += req
+        success += succ
         count += 1
-        if count > 1000:
-            return
+        print(count)
+        if count >= limit:
+            break
+    end = time.time()
+
+    print("Processed {} commits in {} seconds".format(limit, end - start))
+    print("Made {} parse requests, of which {} were successful".format(total, success))
 
 
 repo_path = "data/repos/intellij-community/"
