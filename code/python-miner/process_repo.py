f429fd18f76c0698510e9d1c56130c75a88119e7
Vladimir Kovalenko <v.v.kovalenko@tudelft.nl>
2018-10-15 20:48:57 +0200

    enable incremental processing, keep list of processed commits

--- code/python-miner/process_repo.py
+++ code/python-miner/process_repo.py
@@ -2,6 +2,7 @@ import os
 
 from git import Repo
 from pandas import DataFrame
+import pandas as pd
 import time
 
 repo_name = "intellij-community"
@@ -97,33 +98,63 @@ def explode_repo(project_name, path):
         if total_commits % 10000 == 0:
             print("Counting commits: {}".format(total_commits))
 
+    df = None
+
+    processed_commits = set()
+
+    processed_commits_filename = "data/exploded/{}/processed_commits.csv".format(project_name)
+    temp_data_filename = "data/exploded/{}/infos_incomplete.csv".format(project_name)
+    if os.path.exists(temp_data_filename):
+        print("Found an incomplete dataset: {}".format(temp_data_filename))
+        df = pd.read_csv(temp_data_filename, index_col=None, na_values='')
+        print(df.info(memory_usage='deep', verbose=False))
+        entries = df.to_dict('records')
+        for e in entries:
+            cid = e['commit_id']
+            processed_commits.add(cid)
+        # Not all commits are present in the dataframe (some contain no interesting changes).
+        # To speed up incremental processing, we keep all commit ids that we've encountered on full processing
+        if os.path.exists(processed_commits_filename):
+            print("Found a list of encountered commits: {}".format(processed_commits_filename))
+            processed_commits_df = pd.read_csv(processed_commits_filename, index_col=None, na_values='')
+            commit_entries = processed_commits_df.to_dict('records')
+            for ce in commit_entries:
+                processed_commits.add(ce['id'])
+        print("Will ignore {} already processed commits".format(len(processed_commits)))
+
     limit = 1000000
-    print("{} commits in the repository. Processing {}".format(total_commits, min(limit, total_commits)))
+    commits_to_process = min(limit, total_commits - len(processed_commits))
+    print("{} commits in the repository. Processing {}".format(total_commits, commits_to_process))
 
     processed_count = 0
-    df = None
 
     prev_time = time.time()
     change_infos_chunk = []
     for c in repo.iter_commits():
+        if str(c) in processed_commits:
+            continue
 
         change_infos_chunk += process_commit(c)
-
         processed_count += 1
+        processed_commits.add(str(c))
+
         if processed_count % 5000 == 0:
-            print("Processed {} of {} commits\n".format(processed_count, min(limit, total_commits)))
+            print("Processed {} of {} commits\n".format(processed_count, commits_to_process))
 
             cur_time = time.time()
             print("Last 5000 processed in {} seconds".format(time.time() - prev_time))
             prev_time = cur_time
 
+            new_data = False
+
             if df is None and len(change_infos_chunk) > 0:
                 df = DataFrame.from_records(change_infos_chunk)
             elif len(change_infos_chunk) > 0:
                 df = df.append(DataFrame.from_records(change_infos_chunk))
+                new_data = True
 
-            if df is not None:
-                df.to_csv("data/exploded/{}/infos_incomplete.csv".format(project_name), index=False)
+            if (df is not None) and new_data:
+                df.to_csv(temp_data_filename, index=False)
 
             change_infos_chunk = []
             print(df.info(memory_usage='deep', verbose=False))
@@ -131,7 +162,18 @@ def explode_repo(project_name, path):
             break
 
     if df is not None:
+        if len(change_infos_chunk) > 0:
+            df = df.append(DataFrame.from_records(change_infos_chunk))
+        print(df.info(memory_usage='deep', verbose=False))
         df.to_csv("data/exploded/{}/infos_full.csv".format(project_name), index=False)
+
+    print("Dumping a list of {} already processed commits".format(len(processed_commits)))
+    processed_commits_list = []
+    for c in processed_commits:
+        processed_commits_list.append({'id': c})
+
+    commits_df = DataFrame.from_records(processed_commits_list)
+    commits_df.to_csv(processed_commits_filename, index=False)
 
 
 explode_repo(repo_name, repo_path)
