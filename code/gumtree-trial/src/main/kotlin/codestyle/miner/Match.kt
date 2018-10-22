7e01c96a0d071c0713e1ef0f8c185954648f32c9
Vladimir Kovalenko <v.v.kovalenko@tudelft.nl>
2018-10-22 19:13:46 +0200

    implement method matching

--- code/gumtree-trial/src/main/kotlin/codestyle/miner/Match.kt
+++ code/gumtree-trial/src/main/kotlin/codestyle/miner/Match.kt
@@ -1,9 +1,44 @@
 package codestyle.miner
 
+import com.github.gumtreediff.client.Run
+import com.github.gumtreediff.matchers.Matchers
 import com.github.gumtreediff.tree.TreeContext
 
 fun getMethodMappings(treeBefore: TreeContext, treeAfter: TreeContext): Collection<MethodMapping> {
-    throw NotImplementedError("Not implemented yet")
+    val infosBefore = getMethodInfos(treeBefore)
+    val infosAfter = getMethodInfos(treeAfter)
+
+    Run.initGenerators()
+
+    val matcher = Matchers.getInstance().getMatcher(treeBefore.root, treeAfter.root)
+    matcher.match()
+    val gtMappings = matcher.mappings
+
+    val mappings: MutableSet<MethodMapping> = HashSet()
+
+    infosBefore.forEach {
+        val dst = gtMappings.getDst(it.node)
+        if (dst == null) {
+            mappings.add(MethodMapping(it, null))
+            return@forEach
+        }
+        val dstInfo = infosAfter.firstOrNull { info -> info.node == dst }
+        if (dstInfo == null) {
+            println("Method node $it mapped to unknown node $dst by GumTree")
+            return@forEach
+        }
+
+        mappings.add(MethodMapping(it, dstInfo))
+    }
+
+    infosAfter.forEach {
+        val src = gtMappings.getSrc(it.node)
+        if (src == null) {
+            mappings.add(MethodMapping(null, it))
+        }
+    }
+
+    return mappings
 }
 
 fun getMethodMappings(filenameBefore: String, filenameAfter: String): Collection<MethodMapping> {
