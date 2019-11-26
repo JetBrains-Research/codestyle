e6899dca91b9da577e2ced254dc7616772f60782
egor-bogomolov <msicar.ot.on.yas@gmail.com>
2019-11-26 20:56:39 +0300

    Clean script for representations data

---
+++ code/runner/clean_representations.py
@@ -0,0 +1,21 @@
+import shutil
+import os
+
+
+def rm_folder(path):
+    shutil.rmtree(path, ignore_errors=True)
+
+
+def clean_generated_data():
+    out_root = "../pythonminer/out"
+    for filename in os.listdir(out_root):
+        path = os.path.join(out_root, filename)
+        if os.path.isdir(path):
+            print("Cleaning generated data in", path)
+            generated_root = os.path.join(path, "generated_data")
+            if os.path.exists(generated_root):
+                rm_folder(generated_root)
+
+
+clean_generated_data()
+
