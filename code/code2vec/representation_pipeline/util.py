91c71ca2ee213a67af86fffb1554dbc2e3721ffd
egor-bogomolov <msicar.ot.on.yas@gmail.com>
2019-11-26 20:56:20 +0300

    Store all generated data in a separate directory

--- code/code2vec/representation_pipeline/util.py
+++ code/code2vec/representation_pipeline/util.py
@@ -6,17 +6,21 @@ import pandas as pd
 class ProcessedFolder:
     def __init__(self, folder):
         self.folder = folder
+        self.generated_folder = os.path.join(folder, "generated_data")
+        if not os.path.exists(self.generated_folder):
+            os.mkdir(self.generated_folder)
+
         self.change_metadata_file = os.path.join(folder, "change_metadata.csv")
         self.method_ids_file = os.path.join(folder, "method_ids.csv")
         self.node_types_file = os.path.join(folder, "node_types.csv")
         self.path_ids_file = os.path.join(folder, "path_ids.csv")
         self.tokens_file = os.path.join(folder, "tokens.csv")
-        self.entity_dict = os.path.join(folder, "entity_dict.pkl")
-        self.reversed_entity_dict = os.path.join(folder, "reversed_entity_dict.pkl")
-        self.resolved_entities = os.path.join(folder, "resolved_entities.csv")
-        self.author_occurrences = os.path.join(folder, "author_occurrences.pkl")
-        self.change_occurrences = os.path.join(folder, "change_occurrences.pkl")
-        self.author_to_changes = os.path.join(folder, "author_to_changes.pkl")
+        self.entity_dict = os.path.join(self.generated_folder, "entity_dict.pkl")
+        self.reversed_entity_dict = os.path.join(self.generated_folder, "reversed_entity_dict.pkl")
+        self.resolved_entities = os.path.join(self.generated_folder, "resolved_entities.csv")
+        self.author_occurrences = os.path.join(self.generated_folder, "author_occurrences.pkl")
+        self.change_occurrences = os.path.join(self.generated_folder, "change_occurrences.pkl")
+        self.author_to_changes = os.path.join(self.generated_folder, "author_to_changes.pkl")
         self.file_changes = [os.path.join(folder, f) for f in os.listdir(folder) if f.startswith("file_changes")]
         self._time_buckets_split = "time_buckets_split_{}.pkl"
         self._entity_packs = "entity_packs_{}.pkl"
@@ -26,10 +30,10 @@ class ProcessedFolder:
         self._vectorization_file = "vectorization_{}_packs_{}_samples.csv"
 
     def time_buckets_split(self, n_buckets):
-        return os.path.join(self.folder, self._time_buckets_split.format(n_buckets))
+        return os.path.join(self.generated_folder, self._time_buckets_split.format(n_buckets))
 
     def entity_packs(self, pack_size):
-        return os.path.join(self.folder, self._entity_packs.format(pack_size))
+        return os.path.join(self.generated_folder, self._entity_packs.format(pack_size))
 
     def n_tokens(self):
         if self._n_tokens is None:
@@ -44,10 +48,10 @@ class ProcessedFolder:
         return self._n_paths
 
     def trained_model_folder(self, pack_size: int, min_samples: int):
-        folder = os.path.join(self.folder, self._trained_model_folder.format(pack_size, min_samples))
+        folder = os.path.join(self.generated_folder, self._trained_model_folder.format(pack_size, min_samples))
         if not os.path.exists(folder):
             os.mkdir(folder)
         return folder
 
     def vectorization_file(self, pack_size: int, min_samples: int):
-        return os.path.join(self.folder, self._vectorization_file.format(pack_size, min_samples))
+        return os.path.join(self.generated_folder, self._vectorization_file.format(pack_size, min_samples))
