be2b6aac69fb8afa11bf5b23700af4e7a7077f38
egor-bogomolov <msicar.ot.on.yas@gmail.com>
2019-11-27 23:48:14 +0300

    Multiple runs per extraction

--- code/runner/extract_representations.py
+++ code/runner/extract_representations.py
@@ -3,7 +3,7 @@ import subprocess
 
 def run():
     os.chdir("../code2vec/representation_pipeline")
-    subprocess.run(["python3", "run_all.py", "--pack_size", "4", "--embedding_size", "8", "--min_samples", "4", "--n_time_buckets", "2", "--n_runs", "1"])
+    subprocess.run(["python3", "run_all.py", "--pack_size", "4", "--embedding_size", "8", "--min_samples", "4", "--n_time_buckets", "2", "--n_runs", "10"])
 
 run()
 
