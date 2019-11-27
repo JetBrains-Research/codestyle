4110c996dc906f301d23c22b25081a2eeee802eb
egor-bogomolov <msicar.ot.on.yas@gmail.com>
2019-11-27 23:36:18 +0300

    Fix random seed

--- code/code2vec/representation_pipeline/run_all.py
+++ code/code2vec/representation_pipeline/run_all.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+import numpy as np
 
 from argparse import ArgumentParser
 
@@ -6,12 +7,19 @@ from get_representations import get_representations
 from merge_aliases_naive import merge_aliases_naive
 from util import ProcessedFolder
 
+
+def fix_seed(seed: int):
+    np.random.seed(seed)
+    tf.set_random_seed(seed)
+
+
 parser = ArgumentParser()
 
 parser.add_argument("--pack_size", type=int, required=True)
 parser.add_argument("--embedding_size", type=int, required=True)
 parser.add_argument("--min_samples", type=int, default=0)
 parser.add_argument("--n_time_buckets", type=int, required=True)
+parser.add_argument("--n_runs", type=int, required=True)
 
 args = parser.parse_args()
 
@@ -19,5 +27,7 @@ projects = [l.strip() for l in open("../../pythonminer/projects.txt", "r").readl
 for p in projects:
     project_folder = ProcessedFolder("../../pythonminer/out/" + p + "/")
     merge_aliases_naive(project_folder)
-    tf.reset_default_graph()
-    get_representations(project_folder, args.pack_size, args.embedding_size, args.min_samples, args.n_time_buckets)
+    for n_run in range(args.n_runs):
+        tf.reset_default_graph()
+        fix_seed(n_run)
+        get_representations(project_folder, args.pack_size, args.embedding_size, args.min_samples, args.n_time_buckets)
