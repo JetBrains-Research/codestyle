a123b5856b6c3ba2174e8d684bd20cad05d65105
egor-bogomolov <msicar.ot.on.yas@gmail.com>
2019-12-12 23:44:46 +0300

    Modify extraction script

--- code/runner/extract_representations.py
+++ code/runner/extract_representations.py
@@ -3,7 +3,7 @@ import subprocess
 
 def run():
     os.chdir("../code2vec/representation_pipeline")
-    subprocess.run(["python3", "run_all.py", "--pack_size", "4", "--embedding_size", "8", "--min_samples", "1000", "--n_time_buckets", "20", "--n_runs", "30"])
+    subprocess.run(["python3", "run_all.py", "--pack_size", "4", "--embedding_size", "8", "--min_samples", "1", "--n_time_buckets", "20", "--n_runs", "30", "--mask_tokens"])
 
 run()
 
